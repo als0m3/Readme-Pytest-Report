bb867d2807a98657a3a970626e60a589ed0fd03b
Antonin <home@do-not-try.local>
2022-08-21 22:18:09 +0200

    :construction: debug  main.py

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
 import os
 
 print("START PYTHON MAIN FILE")
-print(os.environ["INPUT_USERNAME"])
-print(os.environ["INPUT_USER_TOKEN"])
-print(os.environ["INPUT_REPO"])
-print(os.environ["INPUT_BRANCH"])
